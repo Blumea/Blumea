be248a8a0cffab61523418279f131feab13e795e
Akash Chouhan <akash.c1500@gmail.com>
2022-04-19 23:24:38 +0530

    :bulb: update index and package v1.0.3

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+/*******************************************
+ * copyright: @github.com/Blumea  
+ * authors: @akashchouhan16 @singhtaran1005
+ * *****************************************
+*/ 
 require('dotenv').config()
-const murmurhash = require('murmurhash')
+const version = require('./package.json').version
+
 const bloomFilterBasic = require('./bloomFilterBasic/main')
 const {buildVector, setBit, getBit} = require('./bloomFilterBasic/bitarray')
+const bitArray = {buildVector, setBit, getBit}
 
-const test = ()=>{
-    console.log('[*]Blumea Package is ready! :)')
-}
 
-module.exports = {test, bloomFilterBasic, buildVector}
+module.exports = {bloomFilterBasic, bitArray}
