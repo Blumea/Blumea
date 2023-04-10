bc8166886bef6b5a2bd6518c3f747e2e8dc6138b
Akash Chouhan <akash.c1500@gmail.com>
2023-04-10 18:38:43 +0530

    :pencil2: countingBloom log changes

--- countingBloom/main.js
+++ countingBloom/main.js
@@ -5,9 +5,10 @@
 */
 const murmurhash = require('murmurhash')
 const { isLogsActive } = require('../logger/logger')
+const { log, warn } = require('console')
 
 const styles = require('terminal-styles')
-const { cyan, x, red, bold, blackBright } = styles
+const { x, red, bold, blue } = styles
 
 
 class CountingBloomFilter {
@@ -24,14 +25,24 @@ class CountingBloomFilter {
     // Bloom filter instance initialization:
     constructor(items_count, false_positive) {
         this.logger = isLogsActive();
-        this.items_count = items_count;
-        // Prevent invalid false positive rate inputs:
-        if (false_positive <= 0.0 || false_positive >= 0.999 || false_positive < 0.0001) {
-            false_positive = 0.01; //set to lowest safe permitted value.
-            if (this.logger)
-                console.log(styles`${red}${bold}[*]Invalid False positive rate. Updated to: 0.01${x}${x}`)
+
+        // Prevent invalid item_count:
+        if (!items_count || Number(items_count) > 7_000_000 || items_count <= 0) {
+            items_count = 10000; //set to lowest safe permitted value.
+            if (this.logger) {
+                log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + styles`${red}${bold}Invalid Item count. Updated to: 10000${x}${x}]`)
+            }
         }
 
+        // Prevent invalid false positive rate:
+        if (!false_positive || false_positive >= 0.999 || false_positive < 0.01) {
+            false_positive = 0.01; //set to lowest safe permitted value.
+            if (this.logger) {
+                log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + styles`${red}${bold}Invalid False positive rate. Updated to: 0.01${x}${x}]`)
+            }
+        }
+
+        this.items_count = items_count;
         this.false_positive = false_positive;
         this.size = this.getSize(this.item_count, this.false_positive)
         this.hash_count = this.getHashCount(this.size, this.items_count)
@@ -42,6 +53,10 @@ class CountingBloomFilter {
                 count_bit: 0, //count bit for specific element
                 values: []
             }
+
+        if (this.logger) {
+            log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + styles`${blue}${bold}CountingBloomFilter instance created.${x}${x}]`)
+        }
     }
 
     // Primary Method definitions:
@@ -56,7 +71,7 @@ class CountingBloomFilter {
             element_count = this.bit_set[index].count_bit;
         }
         if (this.logger) {
-            console.log(styles`${cyan}${bold}[*]Element Inserted ${x}${red}=> ${x}${x}` + element + `(count = ${element_count})`)
+            log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + element + styles`${blue}${bold} added to the filter${x}${x}, ` + `Count: ` + element_count + `]`)
         }
     }
 
@@ -67,14 +82,23 @@ class CountingBloomFilter {
                 return false
             }
         }
-        if (this.logger)
-            console.log(styles`${blackBright}${bold}[*]Element exists. ${x}${x}`)
+
+        if (this.logger) {
+            log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + element + styles`${blue}${bold} already exists${x}${x}]`)
+        }
         return true
     }
 
 
     // secondary utility methods to access or update the bloom filter parameters.
     updateItemCount(newItemCount) {
+        // Prevent invalid item_count:
+        if (!newItemCount || Number(newItemCount) > 7_000_000 || newItemCount <= 0) {
+            newItemCount = 10000;
+            if (this.logger) {
+                log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + styles`${red}${bold}Invalid Item count. Updated to: 10000${x}${x}]`)
+            }
+        }
         this.items_count = newItemCount
         this.size = this.getSize(this.item_count, this.false_positive)
         this.hash_count = this.getHashCount(this.size, this.items_count)
@@ -82,14 +106,16 @@ class CountingBloomFilter {
 
         for (let i = 0; i < this.size; i++)
             this.bit_set[i] = 0
-        if (this.logger)
-            console.log(styles`${cyan}${bold}[*]Item Count updated to:${x} ${x}` + this.items_count)
+        if (this.logger) {
+            log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + styles`${blue}${bold}Item Count updated to: ${x}${x}` + this.items_count + `]`)
+        }
     }
 
     updateFalsePositiveRate(newFalsePostive) {
-        if (newFalsePostive <= 0.0 || newFalsePostive >= 0.999) {
-            if (this.logger)
-                console.log(styles`${red}${bold}[*]Invalid False positive rate. Updated to: 0.01${x}${x}`)
+        if (!newFalsePostive || newFalsePostive <= 0.0 || newFalsePostive >= 0.999) {
+            if (this.logger) {
+                log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + styles`${red}${bold}Invalid False positive rate. Updated to: 0.01${x}${x}]`)
+            }
             newFalsePostive = 0.01;
         }
         this.false_positive = newFalsePostive;
@@ -99,8 +125,9 @@ class CountingBloomFilter {
 
         for (let i = 0; i < this.size; i++)
             this.bit_set[i] = 0
-        if (this.logger)
-            console.log(styles`${cyan}${bold}[*]False positive rate updated to:${x} ${x}` + this.false_positive)
+        if (this.logger) {
+            log(`[type: ` + styles`${blue}${bold}Counting Bloom${x}${x}, ` + `log: ` + styles`${blue}${bold}False positive rate updated to:${x}${x}` + this.false_positive + `]`)
+        }
     }
 
     getHashFunctionCount() {
